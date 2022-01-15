45440c71efcfb7399de47f9209f293dd97eb90bb
Donglun He <92890736+DonglunHe@users.noreply.github.com>
2022-01-15 13:12:39 -0500

    homepage

--- MeetCIT/cal/views.py
+++ MeetCIT/cal/views.py
@@ -11,8 +11,10 @@ from .models import *
 from .utils import Calendar
 from .forms import EventForm
 
+
 def index(request):
     return HttpResponse('hello')
+
 
 class CalendarView(generic.ListView):
     model = Event
@@ -28,11 +30,20 @@ class CalendarView(generic.ListView):
         context['next_month'] = next_month(d)
         return context
 
+
+def homepage(request):
+    earliest_slots_list = Event.objects.order_by('-start_date')
+    context = {'earliest_slots_list': earliest_slots_list}
+
+    return render(request, 'homepage/homepage.html', context)
+
+
 def get_date(req_month):
     if req_month:
         year, month = (int(x) for x in req_month.split('-'))
         return date(year, month, day=1)
     return datetime.today()
+
 
 def prev_month(d):
     first = d.replace(day=1)
@@ -40,13 +51,15 @@ def prev_month(d):
     month = 'month=' + str(prev_month.year) + '-' + str(prev_month.month)
     return month
 
+
 def next_month(d):
     days_in_month = calendar.monthrange(d.year, d.month)[1]
     last = d.replace(day=days_in_month)
     next_month = last + timedelta(days=1)
     month = 'month=' + str(next_month.year) + '-' + str(next_month.month)
     return month
-    
+
+
 @login_required
 def event(request, event_id=None):
     instance = Event()
