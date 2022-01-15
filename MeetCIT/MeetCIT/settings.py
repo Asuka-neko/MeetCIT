af07f1cd686fae68853c030331ea229561c3566a
Asuka-neko <68123945+Asuka-neko@users.noreply.github.com>
2022-01-15 00:42:49 -0500

    add calendar

--- MeetCIT/MeetCIT/settings.py
+++ MeetCIT/MeetCIT/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'cal.apps.CalConfig',
     'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
