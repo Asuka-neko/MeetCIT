f28b40dfbeb80ea86d97c55848f749181e51a2f1
Asuka-neko <68123945+Asuka-neko@users.noreply.github.com>
2022-01-15 15:39:22 -0500

    remove dashboard

--- MeetCIT/users/urls.py
+++ MeetCIT/users/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path, include
-from users.views import dashboard, register
+from users.views import register
 
 app_name = 'users'
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
-    path('dashboard/', dashboard, name='dashboard'),
     path('register/', register, name='register'),
 ]
