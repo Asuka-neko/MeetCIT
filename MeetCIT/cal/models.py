ede5df77b012ba5f7bc5666cfb888b95d0eaa95c
Donglun He <92890736+DonglunHe@users.noreply.github.com>
2022-01-15 12:12:13 -0500

    Update models.py

--- MeetCIT/cal/models.py
+++ MeetCIT/cal/models.py
@@ -1,11 +1,35 @@
 from django.db import models
 from django.urls import reverse
+from django.utils import timezone
+
 
 class Event(models.Model):
     mentor = models.CharField(max_length=200)
     zoom_link = models.TextField()
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
+    start_date = models.DateTimeField('start date')
+    end_date = models.DateTimeField('end date')
+    avaliable = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.mentor
+
+    def __str__(self):
+        return self.zoom_link
+
+    def __str__(self):
+        return self.start_date
+
+    def __str__(self):
+        return self.end_date
+
+    def is_available(self):
+        return (self.start_date > timezone.now and self.available)
+
+    def has_started(self):
+        return (self.start_date <= timezone.now <= self.end_date)
+
+    def has_ended(self):
+        return (self.end_date < timezone.now)
 
     @property
     def get_html_url(self):
