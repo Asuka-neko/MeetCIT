66bb6104542f09d3b3aa82fb90cf53883678fc4b
Asuka-neko <68123945+Asuka-neko@users.noreply.github.com>
2022-01-15 00:26:40 -0500

    update

--- MeetCIT/MeetCIT/settings.py
+++ MeetCIT/MeetCIT/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'users',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- MeetCIT/users/urls.py
+++ MeetCIT/users/urls.py
@@ -1,10 +1,8 @@
-import imp
-from unicodedata import name
 from django.urls import path, include
 from users.views import dashboard, register
 
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('dashboard/', dashboard, name='dashboard'),
-    path('register/', register, name='register')
+    path('register/', register, name='register'),
 ]
